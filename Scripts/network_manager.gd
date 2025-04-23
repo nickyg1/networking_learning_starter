4f20c757eaee36e74ff36e785ea8e6c606e9f2c2
Patrick Larsen <RoyasCoding@gmail.com>
2025-04-23 14:15:10 -0700

    Episode 3

--- Scripts/network_manager.gd
+++ Scripts/network_manager.gd
@@ -2,6 +2,8 @@ class_name NetworkManager extends Node
 
 @export var ip : String = "127.0.0.1"
 @export var port : int = 9999
+@export var number_of_transfer_channels : int = 3
+@export var max_clients : int = 4
 
 var multiplayer_peer = ENetMultiplayerPeer.new()
 
@@ -15,7 +17,7 @@ func _ready() -> void:
 		_connect_client()
 
 func _create_server():
-	multiplayer_peer.create_server(port)
+	multiplayer_peer.create_server(port, , number_of_transfer_channels)
 	multiplayer.multiplayer_peer = multiplayer_peer
 	
 	multiplayer.peer_connected.connect(_on_peer_connected)
@@ -29,10 +31,18 @@ func _connect_client():
 
 func _on_peer_connected(peer_id : int):
 	print("peer with id %s connected" % peer_id)
+	_send_rpc(peer_id)
 
 func _on_connected_to_server():
 	print("Ive connected to the server")
-	
+
 
 func _on_peer_disconnect(peer_id : int):
 	print("peer with id %s disconnected" % peer_id)
+
+func _send_rpc(reciver_id : int):
+	_my_rpc.rpc_id(reciver_id)
+
+@rpc("any_peer", "call_local", "unreliable", 0)
+func _my_rpc():
+	print("Called by %s on %s" % [multiplayer.get_remote_sender_id(), multiplayer.get_unique_id()])
