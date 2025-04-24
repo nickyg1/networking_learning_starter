15f2134d0e58015c3833068e53c29fb5ab521519
Patrick Larsen <iamjackfornow@gmail.com>
2025-04-24 16:45:44 -0700

    Revert "Episode 2"
    This reverts commit 5cee7c97a582ccba789d921645bf606d14a214c4.

--- Scripts/character_body_3d.gd
+++ Scripts/character_body_3d.gd
@@ -4,15 +4,43 @@ extends CharacterBody3D
 @onready var navigation_agent: NavigationAgent3D = get_node("NavigationAgent3D")
 @onready var body: Node3D = get_node("Body")
 @onready var body_ap: AnimationPlayer = get_node("Body/AnimationPlayer")
-@onready var network_manager : NetworkManager = get_node("/root/Network_Manager")
 @export var cursor_dot : PackedScene;
 
 var current_cursor_dot : MeshInstance3D = null
 
+var multiplayer_peer = ENetMultiplayerPeer.new()
+
 func _ready() -> void:
+	var args : PackedStringArray = OS.get_cmdline_args()
+	
+	if args.has("server"):
+		# create server
+		_create_server()
+	else:
+		# connect client
+		_connect_client()
+	
 	navigation_agent.velocity_computed.connect(Callable(_on_velocity_computed))
 	navigation_agent.target_reached.connect(Callable(_on_arrived_at_target))
 
+
+func _create_server():
+	multiplayer_peer.create_server(9999)
+	multiplayer.multiplayer_peer = multiplayer_peer
+	
+	multiplayer.peer_connected.connect(_on_peer_connected)
+
+func _connect_client():
+	multiplayer_peer.create_client("127.0.0.1", 9999)
+	multiplayer.multiplayer_peer = multiplayer_peer
+	
+	multiplayer.connected_to_server.connect(_on_connected_to_server)
+
+func _on_peer_connected(peer_id : int):
+	print("peer with id %s connected" % peer_id)
+
+func _on_connected_to_server():
+	print("Ive connected to the server")
 
 @rpc("any_peer", "call_local", "unreliable")
 func set_movement_target(movement_target: Vector3):
@@ -44,9 +72,6 @@ func _on_arrived_at_target():
 		current_cursor_dot.queue_free()
 
 func _input(event: InputEvent) -> void:
-	if !network_manager.multiplayer.is_server():
-		return
-	
 	if event is InputEventMouseButton && event.is_pressed():
 		if event.button_index != MOUSE_BUTTON_LEFT:
 			return
