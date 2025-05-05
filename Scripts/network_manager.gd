78f98892452ec50595ae36a1f15b7a96b6810532
Patrick Larsen <RoyasCoding@gmail.com>
2025-05-05 11:38:56 -0700

    Episode 7 & 8
    Forgot to push E7 changes

--- Scripts/network_manager.gd
+++ Scripts/network_manager.gd
@@ -27,6 +27,9 @@ var network_id : int
 var is_server : bool = false
 var network_started : bool = false
 
+## takes requester_id : int and spawn_args : Dictionary
+var validate_spawn_callable : Callable
+
 signal on_server_started
 signal on_connection_failed
 
@@ -62,11 +65,46 @@ func _connect_client():
 	multiplayer.connected_to_server.connect(_on_connected_to_server)
 
 func _on_peer_connected(peer_id : int):
-	connected_players.append(_create_connected_player(peer_id))
 	print("peer with id %s connected" % peer_id)
+	
+	for player in connected_players:
+		if !is_instance_valid(player):
+			connected_players.erase(player)
+		
+		if player.network_id == peer_id:
+			for network_object in player.players_objects:
+				if !is_instance_valid(network_object): continue
+				network_object._destroy_network_object.rpc()
+				
+			connected_players.erase(player)
+			continue
+		
+		for network_object in player.players_objects:
+			if network_object.resource_path.is_empty():
+				network_object._initialize_network_object.rpc(network_object.owner_id, network_object._get_transforms())
+			else:
+				_network_spawn_object.rpc_id(peer_id, network_object.owner_id, network_object.spawn_args)
+				network_object._initialize_network_object.rpc_id(peer_id, network_object.owner_id, network_object._get_transforms())
+			
+	connected_players.append(_create_connected_player(peer_id))
 
 func _on_peer_disconnected(peer_id : int):
 	print("peer with id %s disconnected" % peer_id)
+	
+	var disconnected_player : ConnectePlayer
+	for player in connected_players:
+		if player.network_id == peer_id:
+			disconnected_player = player
+			break
+	
+	if !is_instance_valid(disconnected_player):
+		return
+	
+	for network_object in disconnected_player.players_objects:
+		network_object._destroy_network_object.rpc()
+	
+	if connected_players.has(disconnected_player):
+		connected_players.erase(disconnected_player)
 
 func _on_connected_to_server():
 	network_id = multiplayer.get_unique_id()
@@ -86,6 +124,9 @@ func _register_network_object(network_object : NetworkObject):
 	
 	if !is_server && !network_object._is_owner():
 		return
+	
+	if is_server:
+		network_object._initialize_network_object.rpc(network_object.owner_id, network_object._get_transforms())
 	
 	for player in connected_players:
 		if player.network_id != network_object.owner_id:
@@ -108,3 +149,88 @@ func _switch_network_object(new_owner : int, network_object : NetworkObject):
 				player.players_objects.erase(network_object)
 		if player.network_id == new_owner:
 			player.players_objects.append(network_object)
+
+func _remove_network_object(network_object : NetworkObject):
+	for player in connected_players:
+		if player.network_id == network_object.owner_id:
+			if player.players_objects.has(network_object):
+				player.players_objects.erase(network_object)
+				return
+
+func _request_spawn_helper(resource_path : String, args : Dictionary = {}):
+	
+	if resource_path is not String:
+		push_warning("the resource path is not a string")
+		return
+	
+	if !ResourceLoader.exists(resource_path) || !resource_path.begins_with("res://"):
+		push_warning("Invalid resource path %s" % resource_path)
+		return
+		
+	
+	var dict : Dictionary = {
+		"resource_path" : resource_path,
+		"args" : args
+	}
+	
+	_request_spawn_object.rpc_id(1, dict)
+
+## overridable function
+func _spawn_object(owner_id : int, spawn_args : Dictionary):
+	var resource_path : String = spawn_args["resource_path"]
+	
+	var obj = load(resource_path).instantiate()
+	
+	if !is_instance_valid(obj):
+		push_error("Failed to instantiate: %s" % resource_path)
+		return
+	
+	if obj is NetworkObject:
+		obj.resource_path = resource_path
+		obj.owner_id = owner_id
+		obj.spawn_args = spawn_args
+		
+	
+	get_tree().current_scene.add_child(obj)
+
+@rpc("any_peer", "call_local", "reliable")
+func _request_spawn_object(spawn_args : Dictionary):
+	var request_id = multiplayer.get_remote_sender_id()
+	
+	if !spawn_args.has("resource_path"):
+		push_warning("No resource path provided")
+		return
+		
+	var resource_path = spawn_args["resource_path"]
+	
+	if resource_path is not String:
+		push_warning("the resource path is not a string")
+		return
+	
+	if !ResourceLoader.exists(resource_path) || !resource_path.begins_with("res://"):
+		push_warning("Invalid resource path %s" % resource_path)
+		return
+	
+	if validate_spawn_callable:
+		if validate_spawn_callable.call(request_id, spawn_args):
+			_network_spawn_object.rpc()
+	else:
+		_network_spawn_object.rpc()
+	
+
+@rpc("authority", "call_local", "reliable")
+func _network_spawn_object(
+	owner_id : int,
+	spawn_args : Dictionary
+):
+	var resource_path = spawn_args["resource_path"]
+	
+	if resource_path is not String:
+		push_warning("the resource path is not a string")
+		return
+	
+	if !ResourceLoader.exists(resource_path) || !resource_path.begins_with("res://"):
+		push_warning("Invalid resource path %s" % resource_path)
+		return
+	
+	_spawn_object(owner_id, spawn_args)
--- Scripts/network_object.gd
+++ Scripts/network_object.gd
@@ -4,11 +4,15 @@ class_name NetworkObject extends Node
 
 var owner_id : int = 1
 var has_begun_initialiation : bool = false
+var resource_path : String = ""
+var spawn_args : Dictionary = {}
 
 var validate_ownership_change_callable : Callable
+var validate_destroy_request_callable : Callable
 
 signal on_network_ready()
 signal on_ownership_change(old_owner : int, new_owner : int)
+signal on_network_object_destroy
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,7 +28,6 @@ func _on_network_started():
 	has_begun_initialiation = true
 	
 	network_manager._register_network_object(self)
-	on_network_ready.emit()
 	
 	#if !network_manager.is_server:
 		#_request_ownership.rpc_id(1)
@@ -35,6 +38,30 @@ func _on_network_started():
 
 func _is_owner() -> bool:
 	return owner_id == network_manager.network_id
+
+func _get_transforms() -> Dictionary:
+	var dict : Dictionary = {}
+	
+	var children := _get_all_children_transforms(self)
+	
+	for child in children:
+		dict.set(child.get_path(), child.transform) 
+		
+	return dict
+
+func _get_all_children_transforms(node : Node) -> Array[Node]:
+	var nodes : Array[Node] = []
+	
+	for n in node.get_children():
+		if n.get_child_count() > 0:
+			if n is Node2D || n is Node3D:
+				nodes.append(n)
+			
+			nodes.append_array(_get_all_children_transforms(n))
+		elif n is Node2D || n is Node3D:
+			nodes.append(n)
+			
+	return nodes
 
 @rpc("any_peer", "call_local", "reliable")
 func _request_ownership():
@@ -65,3 +92,34 @@ func _change_owner(new_owner : int):
 	on_ownership_change.emit(owner_id, new_owner)
 	print("old owner: %s new owner: %s" % [owner_id, new_owner])
 	owner_id = new_owner
+
+@rpc("authority", "call_remote", "reliable")
+func _initialize_network_object(owner_id : int, transforms : Dictionary):
+	self.owner_id = owner_id
+	
+	var children_transforms := _get_all_children_transforms(self)
+	for child in children_transforms:
+		var child_path = child.get_path()
+		if transforms.has(child_path):
+			child.transform = transforms[child_path]
+	
+	on_network_ready.emit()
+
+@rpc("any_peer", "call_local", "reliable")
+func _request_destroy_network_object():
+	var sender_id : int = network_manager.multiplayer.get_remote_sender_id()
+	
+	if validate_destroy_request_callable:
+		if validate_destroy_request_callable.call(sender_id):
+			_destroy_network_object.rpc()
+	else:
+		_destroy_network_object.rpc()
+
+@rpc("authority", "call_local", "reliable")
+func _destroy_network_object():
+	if _is_owner() || network_manager.is_server:
+		#remove network object from players object list
+		pass
+	
+	on_network_object_destroy.emit()
+	queue_free()
